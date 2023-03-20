b10147c4f9c6038f982876f79303bd90b7a296b1
aadi0105 <mishraaditya8782@gmail.com>
2023-03-20 11:31:41 +0530

    update from aditya

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git add .
-git commit -m 'update drom aditya'
+git commit -m 'update from aditya'
 git push origin master
 ssh -i bastion ec2-user@52.54.244.199 '~/redis_proxy/restart.sh'
